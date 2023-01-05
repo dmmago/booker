13d38e8a4d9db46431209c731c4e9901cd62fdfc
EC2 Default User <ec2-user@ip-172-31-40-117.ap-northeast-1.compute.internal>
2023-01-05 09:19:17 +0000

    kadai2_index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,39 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+    #投稿とindexが同画面ならここ記載？@books=Book.all
+    
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to '/books'
   end
 
   def index
+    #投稿とindexが同画面ならここ記載？@book=Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id]) 
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+    
+    
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
